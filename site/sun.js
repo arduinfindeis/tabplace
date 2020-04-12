9b99fc1f88c2431f5d20e5a4c33c86d4287d4e67
gmff <52667404+gmff@users.noreply.github.com>
2020-04-12 11:06:24 +0200

    add sun.js

---
+++ site/sun.js
@@ -0,0 +1,24 @@
+/* html
+
+
+*/
+
+var t = new Date();
+now = Date.getTime();
+
+console.log(t, now);
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+func time_duration(begin, end)
+
+
+sun_up = Date;
+sun_down = 1800;
+sun_path = 
+console.log(width, height);
+
+func suncalc(time, sun_up, sun_down, sun_path) {
+    full_sun = sun_down - sun_up;
+    
+};
